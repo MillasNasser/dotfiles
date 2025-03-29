ae0442fceee999e58a0668266f7c70b8d31b36e5
Millas Nasser <millas5444@gmail.com>
2025-03-29 11:18:45 -0300

    feat: Adicionando instalação termux (Android)
    Adicionando script de setup inicial para o termux, com o intuito de
    configurar rapidamente novos dispositivos
    
    Em meio ao script está sendo gerado um link simbolico para as
    configurações do nvim, usando a pasta nvim do dotfiles
    
    Além disso, foi desativado a página de dashboard do lazyvim

---
+++ termux/install.sh
@@ -0,0 +1,17 @@
+apt update -y && apt upgrade -y
+pkg install git -y
+pkg install python -y
+pkg install openssh -y
+pkg install lazygit -y
+pkg install nodejs -y
+pkg install which -y
+pkg install neovim -y
+mkdir projetos
+git clone https://github.com/MillasNasser/dotfiles.git
+mv dotfiles/ projetos/dotfiles
+ln -s ~/projetos/dotfiles/nvim ~/.config/nvim
+curl -o /data/data/com.termux/files/usr/bin/install-in-mason  https://raw.githubusercontent.com/Amirulmuuminin/setup-mason-for-termux/main/install-in-mason
+chmod +x /data/data/com.termux/files/usr/bin/install-in-mason
+install-in-mason lua-language-server
+sh <(curl -sS https://raw.githubusercontent.com/blackc8/termux-composer/master/install)
+# ssh-keygen -t ed25519 -C "millas5444@gmail.com"
