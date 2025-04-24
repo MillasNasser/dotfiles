92dc7d32c34d46e9f64d1f07b58be54419390196
Millas Nasser Ramsses Avelar <millas.ramsses@stone.com.br>
2025-04-24 17:36:35 -0300

    Adding some extensions and removing startup page from coq and lazy

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -31,7 +31,10 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = {
+        enabled = true,
+        notify = false
+    },
 })
 
 vim.keymap.set({"n"}, "<F2>", "<cmd>Lazy<Cr>", {silent=true})
