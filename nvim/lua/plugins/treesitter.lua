55d83311b38aeeec09e3981aaf1b54c54ff6c724
Millas Nasser Ramsses Avelar <millas.ramsses@stone.com.br>
2025-08-29 14:18:21 -0300

    Adding Treesitter

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,32 @@
+return {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        branch = 'master',
+        lazy = false,
+        build = ":TSUpdate",
+        config = function ()
+            require("nvim-treesitter").setup({
+                ensure_installed = {
+                    "c",
+                    "lua",
+                    "vim",
+                    "vimdoc",
+                    "query",
+                    "markdown",
+                    "markdown_inline"
+                },
+                auto_install = true,
+            })
+
+            require("nvim-treesitter.configs").setup {
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        node_incremental = "v",
+                        node_decremental = "V",
+                    },
+                },
+            }
+        end
+    }
+}
